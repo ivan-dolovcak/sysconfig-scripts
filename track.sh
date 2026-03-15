f284ae1f89b07cba782b83579dfbdb80b790e284
Ivan Dolovčak <140179231+ivan-dolovcak@users.noreply.github.com>
2026-03-15 22:54:59 +0100

    fix: make track.sh POSIX compliant; comments

--- track.sh
+++ track.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/sh
 
-set -euo pipefail
+set -eu
 
 repo="$HOME/sysconfig"
 fileToTrack="$(realpath "$1")"
 branch="$(cat /etc/hostname)"
 
 # auto-create branch for host if it doesn't exist:
-git -C $repo checkout -b $branch 2>/dev/null || git -C $repo checkout $branch
+if ! git -C "$repo" checkout -b "$branch" 2>/dev/null
+then
+    git -C "$repo" checkout "$branch"
+fi
 
-cp --parents $fileToTrack $repo
-git -C $repo add -f -- "./$fileToTrack"
+# copy the file along with its complete directory structure.
+cp --parents "$fileToTrack" "$repo"
+
+git -C "$repo" add -f -- "./$fileToTrack"
 
 echo "Staged $fileToTrack"
