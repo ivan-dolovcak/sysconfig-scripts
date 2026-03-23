4cd9fc0e3998394420360ea3af4cb002128f3e90
Ivan Dolovčak <140179231+ivan-dolovcak@users.noreply.github.com>
2026-03-23 01:33:25 +0100

    feat: add helper script for generating manifest from scratch

---
+++ bin/genmanifest.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Generate a manifest file from scratch.
+set -eu
+SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+. "$SCRIPT_DIR/../lib/common.sh"
+
+require_root
+
+find "$REPO_PATH" \( $MANIFEST_IGNORE \) -prune -o \
+    \( ! -path "$REPO_PATH" \) -print |
+while IFS= read -r path; do
+    upsert_manifest "$path"
+done
